4ce841c746dce21efe58e854a953866e037ad9a0
tanglin <2196184435@qq.com>
2018-07-30 21:40:19 +0800

    面试

--- view/test.js
+++ view/test.js
@@ -1,21 +1,8 @@
-
-var currying = function(fn) {
-    var args = [].slice.call(arguments, 1);
-
-    return function() {
-        // 主要还是收集所有需要的参数到一个数组中，便于统一计算
-        var _args = args.concat([].slice.call(arguments));
-        return fn.apply(null, _args);
+const res = (function bar () {
+    const a = 12
+    return function () {
+        console.log(a)
     }
-}
-
-var sum = currying(function() {
-    var args = [].slice.call(arguments);
-    return args.reduce(function(a, b) {
-        return a + b;
-    })
-}, 10)
-
-console.log(sum(20, 10));  // 40
-console.log(sum(10, 5));   // 25
-console.log(sum(10, 5, 20, 15));   // 60
+})()
+// const res = bar()
+res()
